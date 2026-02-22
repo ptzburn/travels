92a8d38f7a1a7b96750b1bf95a285572676f31ed
ptzburn <milan@hokkanen.io>
2026-02-22 22:14:29 +0200

    backup

--- src/client/lib/queries/locations.ts
+++ src/client/lib/queries/locations.ts
@@ -18,8 +18,6 @@ export const userLocationsQuery = query(async () => {
 export const userLocationQuery = query(async (slug?: string) => {
   "use server";
 
-  console.log("slug", slug);
-
   if (!slug) {
     throw new Error("Slug is required");
   }
