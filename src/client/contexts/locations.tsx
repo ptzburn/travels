22bd2ec93d41f15d50d3c8664a6d73a051b8f80b
ptzburn <milan@hokkanen.io>
2026-02-22 21:41:23 +0200

    changes again

--- src/client/contexts/locations.tsx
+++ src/client/contexts/locations.tsx
@@ -80,13 +80,9 @@ function LocationsProviderWithSession(props: ParentProps) {
 
 function LocationProviderWithSession(props: ParentProps) {
   const params = useParams();
-  console.log("Location Provider With Session");
-  console.log("params slug", params.slug);
-  const location = createAsync(async () => {
-    console.log("CALL userLocationQuery with", params.slug);
-    if (!params.slug) return [];
-    return await userLocationQuery(params.slug);
-  }, { initialValue: [] });
+  const location = createAsync(() => userLocationQuery(params.slug), {
+    initialValue: [],
+  });
 
   return (
     <LocationsContext.Provider value={location}>
